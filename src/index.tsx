586dd206f8cae31fdafece2730cd406a21831ff8
Ivan Pidko <vany2008@yandex.ru>
2024-08-02 07:21:00 +0300

    errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import style from './index.module.scss';
-import './index.scss';
-import './assets/images/dog.jpg';
+import style from './index.module.scss'
+import './index.scss'
+import './assets/images/dog.jpg'
 
-console.log(style); //использование объекта style важно для попадания стилей в css-бандл
+console.log(style) //использование объекта style важно для попадания стилей в css-бандл
 
 // const numbers:number[] = [2, 3, 5];
 
